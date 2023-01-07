6f5a4583f4b5741f836806ecccde08e168678966
Paul Moore <murashkov.pavel@gmail.com>
2023-01-07 13:49:16 +0400

    Homework 4, creating contact creation test with additional methods and classes

---
+++ Addressbook-web-tests/Addressbook-web-tests/ContactData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook_web_tests
+{
+    internal class ContactData
+    {
+        private string firstName;
+        private string lastName;
+
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+    }
+}
