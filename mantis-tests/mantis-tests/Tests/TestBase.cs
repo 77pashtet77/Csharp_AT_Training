9609a2693817fcb50ccf60f7dd7c5564dac3aadd
Paul Moore <murashkov.pavel@gmail.com>
2023-02-04 16:09:55 +0400

    -Added James usage

--- mantis-tests/mantis-tests/Tests/TestBase.cs
+++ mantis-tests/mantis-tests/Tests/TestBase.cs
@@ -8,45 +8,12 @@ namespace mantis_tests
 {
     public class TestBase
     {
-        public static bool PERFORM_LONG_UI_CHECKS = true;
-
         protected ApplicationManager app;
 
         [OneTimeSetUp]
         public void SetupApplicationManager()
         {
             app = ApplicationManager.GetInstance();
-        }
-
-        public static Random rnd = new Random();
-
-        public static string GenerateRandomString(int max)
-        {
-            int l = Convert.ToInt32(rnd.NextDouble()*max);
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < l; i++)
-            {
-                builder.Append(Convert.ToChar(32 + Convert.ToInt32(rnd.NextDouble() * 65)));
-            }
-            return StringCaseRandomizer(builder.ToString());
-        }
-
-        public static string StringCaseRandomizer(string textToTweakCase)
-        {
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < textToTweakCase.Length; i++)
-            {
-                int l = Convert.ToInt32(rnd.NextDouble() * 2);
-                if (l > 1)
-                {
-                    builder.Append(textToTweakCase.Substring(i, 1).ToLower());
-                }
-                else
-                {
-                    builder.Append(textToTweakCase.Substring(i, 1).ToUpper());
-                }
-            }
-            return builder.ToString();
         }
     }
 }
